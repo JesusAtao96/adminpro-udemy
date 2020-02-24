f8e715b5cd5037dd11feb0028eddc451325b3c13
Jesús Alejandro Atao <jealeat96@gmail.com>
2020-02-24 17:22:49 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,35 +4,22 @@ import { NgModule } from '@angular/core';
 // Rutas
 import { APP_ROUTES } from './app.routes';
 
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       LoginComponent,
-      RegisterComponent,
-      NopagefoundComponent,
-      DashboardComponent,
-      ProgressComponent,
-      Graficas1Component,
-      HeaderComponent,
-      SidebarComponent,
-      BreadcrumbsComponent,
-      PagesComponent
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
+      PagesModule,
       APP_ROUTES
    ],
    providers: [],
