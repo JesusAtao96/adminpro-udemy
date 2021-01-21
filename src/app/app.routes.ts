90fd7beccace8c4aae03a056ee8b788cbc958c90
Jesús Alejandro Atao <jealeat96@gmail.com>
2021-01-21 11:13:40 -0500

    Angular 11.1 Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,4 +20,4 @@ const appRoutes: Routes = [
   { path: '**', component: NopagefoundComponent }
 ];
 
-export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true });
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true, relativeLinkResolution: 'legacy' });
